11fa8c0d5d3c98d230c2ef020b991667acb5fd20
Jan Schroeder <jschroed@c3a3c4.42berlin.de>
2024-05-10 18:23:01 +0200

    started new way of handling signals in heredoc

--- src/executor/heredoc.c
+++ src/executor/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: xiruwang <xiruwang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 17:52:49 by xiwang            #+#    #+#             */
-/*   Updated: 2024/05/09 13:51:55 by xiruwang         ###   ########.fr       */
+/*   Updated: 2024/05/10 18:21:01 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,113 +40,6 @@ void	check_hd(t_cmd *cmd)
 	}
 }
 
-/*
-static int readline_event_hook_heredoc(void) {
-    if (last_received_signal == SIGINT) {
-        rl_replace_line("", 0);
-        rl_done = 1;
-        write(STDOUT_FILENO, "\n", 1);
-        close(rl_instream->_fileno);
-        last_received_signal = 0;
-        return (1);
-    }
-    return (0);
-}
-static int    create_hd(t_cmd *cmd, int expand_sign)
-{
-    int     fd;
-    int     i;
-    char    *line;
-    char    *new;
-
-    fd = open(cmd->hdfile, O_CREAT | O_RDWR | O_TRUNC, 0644);
-
-    line = NULL;
-    i = 1;
-    last_received_signal = 0;  // Initialize last_received_signal to 0
-    while (1)
-    {
-        rl_event_hook = readline_event_hook_heredoc;
-        line = readline("heredoc>");
-        if (rl_event_hook == readline_event_hook_heredoc && last_received_signal == SIGINT)
-            break;
-        rl_event_hook = NULL;
-
-        if (line == NULL || ft_strncmp(line, cmd->delimiter, ft_strlen(cmd->delimiter)) == 0)
-            break;
-        if (expand_sign == 0 && check_valid_dollar(line))
-        {
-            new = expand_simple(line, cmd->data->env, cmd->data);
-            ft_putendl_fd(new, fd);
-            free(new);
-        }
-        else
-            ft_putendl_fd(line, fd);
-        free(line);
-        i++;
-    }
-    if (line == NULL && last_received_signal != SIGINT)
-        printf("minishell: warning: here-document at line %d delimited by end-of-file (wanted `%s')\n", i, cmd->delimiter);
-    free(line);
-    close(fd);
-
-    if (last_received_signal == SIGINT) {
-        unlink(cmd->hdfile);
-        last_received_signal = 0;
-        return (0);
-    }
-    return (1);
-}
-*/
-
-//Reset the user input prompt to a new blank line
-// void	reset_prompt(int sig)
-// {
-// 	(void)sig;
-// 	write(1, "\n", 1);
-// 	rl_on_new_line();
-// 	rl_replace_line("", 0);
-// 	rl_redisplay();
-// }
-
-// void	ignore_sigquit(void)
-// {
-// 	struct sigaction	s_sigact;
-
-// 	ft_memset(&s_sigact, 0, sizeof(s_sigact));
-// 	s_sigact.sa_handler = SIG_IGN;
-// 	sigaction(SIGQUIT, &s_sigact, 0);
-// }
-
-// void init_sig_hd(void)
-// {
-// 	struct sigaction	s_sigact;
-
-// 	ignore_sigquit();
-// 	ft_memset(&s_sigact, 0, sizeof(s_sigact));
-// 	s_sigact.sa_handler = reset_prompt;
-// 	sigaction(SIGINT, &s_sigact, 0);
-// }
-
-// void	print_newline(int sig)
-// {
-// 	(void)sig;
-// 	if (sig == SIGQUIT)
-// 		write(1, "Quit (core dumped)", ft_strlen("Quit (core dumped)"));
-// 	write(1, "\n", 1);
-// 	rl_on_new_line();
-// }
-
-// void	signal_non_interactive(void)
-// {
-// 	struct sigaction	s_sigact;
-
-// 	ft_memset(&s_sigact, 0, sizeof(s_sigact));
-// 	s_sigact.sa_handler = print_newline;
-// 	sigaction(SIGINT, &s_sigact, 0);
-// 	sigaction(SIGQUIT, &s_sigact, 0);
-// }
-
 //if here_doc got interrupted??
 static int	create_hd(t_cmd *cmd, int expand_sign)
 {
@@ -155,6 +48,11 @@ static int	create_hd(t_cmd *cmd, int expand_sign)
 	char	*line;
 	char	*new;
 
+	struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = signal_handler;
+    sigaction(SIGINT, &sa, NULL);
+
 	fd = open(cmd->hdfile, O_CREAT | O_RDWR | O_TRUNC, 0644);
 	line = readline("heredoc>");
 	i = 1;
@@ -162,6 +60,11 @@ static int	create_hd(t_cmd *cmd, int expand_sign)
 	{
 		if (ft_strncmp(line, cmd->delimiter, ft_strlen(cmd->delimiter)) == 0)
 			break;
+		if (last_received_signal)
+		{
+			printf("Received signal %d, breaking loop.\n", last_received_signal);
+			break;
+		}
 		if (expand_sign == 0 && check_valid_dollar(line))
 		{
 			new = expand_simple(line, cmd->data->env, cmd->data);
