70824cb0104a8b604167633072434993888db8ee
Xiru Wang <xiwang@c3a1c7.42berlin.de>
2024-05-18 23:27:28 +0200

    pipe fixed

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -51,34 +51,27 @@ static int	execute_command_pipeline(t_cmd *cmd)
 	current = cmd;
 	while (current)
 	{
-		if (check_hd(current) == 130)//current!!not cmd!!
-			return (130);//try where should i put heredoc??
 		if (current->next)
 			pipe(end);
+		if (check_hd(current) == 130)//current!!not cmd!!
+			return (130);//try where should i put heredoc??
 		cmd->data->pid[i] = fork();
 		if (cmd->data->pid[i] == 0)
-		{
 			setup_child_process(current, end, fd_in);
-		}
+		close(end[1]);
 		if (current->next)
-		{ // Parent Process
-			close(end[1]);  // Close the write end of the pipe in the parent
+		{
 			if (fd_in != STDIN_FILENO)
 				close(fd_in);  // Close the previous read end
 			fd_in = end[0];  // Use the read end of the current pipe in the next iteration
 		}
-		if (current->next == NULL)//added
-		{
-			execute_single_command(current);//added
-			break ;
-		}
 		current = current->next;
 		i++;
 	}
+	reset_stdio(cmd);
 	return (wait_for_processes(cmd->data->pid, cmd->data->cmd_num));
 }
 
-//被 dup2 覆盖的文件描述符会被自动关闭,你不需要手动关闭它们。
 static int	setup_child_process(t_cmd *cmd, int *end, int fd_in)
 {
 	if (fd_in != 0)
@@ -110,44 +103,10 @@ static int    wait_for_processes(int *pids, int num_pids)
     exit_status = 0;
     while (i < num_pids)
     {
-        if (waitpid(pids[i], &status, WNOHANG) == -1)//change 0 to WNOHANG
-		{
-			if (errno == EINTR)//signal interrrupted??
-				exit_status = 130;//not sure
-			free_exit("waitpid", NULL, EXIT_FAILURE);
-		}
-        else if (WIFEXITED(status))
-        {
-            if (i == num_pids - 1)
-                exit_status = WEXITSTATUS(status);
-        }
-        i++;
-    }
+        waitpid(pids[i], &status, 0);
+		i++;
+	}
+    if (WIFEXITED(status))
+        exit_status = WEXITSTATUS(status);
     return (exit_status);
 }
-
-
-// static int	wait_for_processes(int *pids, int num_pids)
-// {
-// 	int	i;
-// 	int	status;
-// 	int	exit_status;
-// 	int	current_status;
-
-// 	i = 0;
-// 	exit_status = 0;
-// 	while (i < num_pids)
-// 	{
-// 		if (waitpid(pids[i], &status, 0) == -1)
-// 			free_exit("waitpid", NULL, EXIT_FAILURE);
-// 		else if (WIFEXITED(status))
-// 		{
-// 			current_status = WEXITSTATUS(status);
-// 			// Update the global exit status if it's the last process or if an error occurred
-// 			if (i == num_pids - 1 || current_status != 0)
-// 				exit_status += current_status;
-// 		}
-// 		i++;
-// 	}
-// 	return (exit_status);
-// }
