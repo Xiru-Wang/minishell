f3737f1141544d7138e49b00ed2e49fd759b84da
Jan Schroeder <jschroed@c3a1c2.42berlin.de>
2024-05-20 18:42:01 +0200

    fix pwd and norm

--- src/builtins/call_pwd.c
+++ src/builtins/call_pwd.c
@@ -1,13 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   call_pwd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 18:36:58 by jschroed          #+#    #+#             */
+/*   Updated: 2024/05/20 18:37:04 by jschroed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 int	call_pwd(t_cmd *cmd)
 {
-	if (cmd->s[1])
-	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-		return (EXIT_FAILURE);
-	}
 	ft_putendl_fd(cmd->data->pwd, STDOUT_FILENO);
 	return (EXIT_SUCCESS);
 }
