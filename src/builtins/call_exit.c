ca64d3b00bbb8df46d59e8aa75a71b3711e321bf
Jan Schroeder <jschroed@c3a1c2.42berlin.de>
2024-05-20 16:02:25 +0200

    fixed call exit, need refactor

--- src/builtins/call_exit.c
+++ src/builtins/call_exit.c
@@ -6,75 +6,96 @@
 /*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 12:37:05 by jschroed          #+#    #+#             */
-/*   Updated: 2024/05/20 08:39:24 by jschroed         ###   ########.fr       */
+/*   Updated: 2024/05/20 16:02:16 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-/**
- * @brief Determine the exit code based on the input string and data.
- *
- * This function takes in a string array and a data structure, and determines
- * the exit code based on the input string. If the second element of the string
- * array is not present, the exit code is set to 0. If the second element is a
- * valid digit, it is converted to an integer and used as the exit code. If the
- * second element is not a valid digit, an error message is printed to stderr
- * and the exit code is set to 255.
- *
- * @param str The input string array.
- * @param data The data structure (not used in this function).
- * @return The determined exit code.
- */
-/* static int determine_exit_code(char **str) */
-/* { */
-/*     int exit_code; */
-/*  */
-/*     if (!str[1]) */
-/*         exit_code = 0; */
-/*     else */
-/*     { */
-/*         if (is_str_digit(str[1])) */
-/*             exit_code = ft_atoi(str[1]); */
-/*         else */
-/*         { */
-/*             ft_putstr_fd("minishell: exit: ", STDERR_FILENO); */
-/*             ft_putstr_fd(str[1], STDERR_FILENO); */
-/*             ft_putstr_fd(": numeric argument required\n", STDERR_FILENO); */
-/*             exit_code = 255; */
-/*         } */
-/*     } */
-/*     return exit_code; */
-/* } */
-
-/**
- * Exits the minishell program with a specified exit code.
- *
- * This function takes in a command structure `cmd` and a data structure `data`,
- * and exits the minishell program with a specified exit code. It first prints
- * "exit" to the standard error output, and then checks if there are too many
- * arguments provided in the command structure `cmd`. If there are too many
- * arguments, it prints an error message to the standard error output and
- * returns `EXIT_FAILURE`.
- *
- * @param cmd A pointer to the command structure containing the arguments for
- * the exit command.
- * @param data A pointer to the data structure containing the data for the
- * minishell program.
- * @return The exit code to be used to exit the minishell program.
- */
 int call_exit(t_cmd *cmd, t_data *data)
 {
 	int last_exit_code;
+	char *arg;
+	int i;
+	int sign;
+	int invalid;
+	unsigned long long num;
+	unsigned long long prev_num;
 
-	last_exit_code = 0;
+	last_exit_code = data->exit_code;
 	if (cmd->s[1])
 	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-		return (EXIT_FAILURE);
+		arg = ft_strtrim(cmd->s[1], " \t");
+		if (ft_strlen(arg) == 0 || (ft_strlen(arg) == 1 && (arg[0] == '+' || arg[0] == '-')))
+		{
+			ft_putstr_fd("bash: line 1: exit: ", STDERR_FILENO);
+			ft_putstr_fd(arg, STDERR_FILENO);
+			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+			free(arg);
+			last_exit_code = 2;
+		}
+		else
+		{
+			i = 0;
+			sign = 1;
+			invalid = 0;
+			if (arg[i] == '+' || arg[i] == '-')
+			{
+				if (arg[i] == '-')
+					sign = -1;
+				i++;
+			}
+			num = 0;
+			while (arg[i])
+			{
+				if (!ft_isdigit(arg[i]))
+				{
+					invalid = 1;
+					break;
+				}
+				prev_num = num;
+				num = num * 10 + (arg[i] - '0');
+				if (num / 10 != prev_num)  // overflow check
+				{
+					invalid = 1;
+					break;
+				}
+				i++;
+			}
+
+			// Special handling for LLONG_MIN
+			if (sign == -1 && strcmp(arg, "-9223372036854775808") == 0)
+			{
+				num = 9223372036854775808ULL;
+				invalid = 0;
+			}
+
+			// Check if the number is too large
+			if (invalid || (sign == 1 && num > LLONG_MAX) || (sign == -1 && num > 9223372036854775808ULL))
+			{
+				ft_putstr_fd("bash: line 1: exit: ", STDERR_FILENO);
+				ft_putstr_fd(arg, STDERR_FILENO);
+				ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+				free(arg);
+				last_exit_code = 2;
+			}
+			else
+			{
+				long long final_num = (sign == -1) ? -((long long)num) : (long long)num;
+				if (cmd->s[2])
+				{
+					ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+					free(arg);
+					return EXIT_FAILURE;
+				}
+				else
+				{
+					last_exit_code = (int)(final_num % 256);
+					free(arg);
+				}
+			}
+		}
 	}
-	ft_putendl_fd("exit", STDERR_FILENO);
-	last_exit_code = data->exit_code;
 	free_data(data);
 	exit(last_exit_code);
 }
