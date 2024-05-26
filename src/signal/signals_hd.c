65256c77a6fec71ce6c02d0daaa31b2bf047ab78
Jan Schroeder <jan.schroeder@oldendorff.com>
2024-05-26 13:41:37 +0200

    fixed sigquit

--- src/signal/signals_hd.c
+++ src/signal/signals_hd.c
@@ -6,7 +6,7 @@
 /*   By: xiruwang <xiruwang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 19:42:08 by jschroed          #+#    #+#             */
-/*   Updated: 2024/05/26 13:04:22 by jschroed         ###   ########.fr       */
+/*   Updated: 2024/05/26 13:41:15 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,16 @@ int	readline_event_hook_hd(void)
 void	init_signals_hd(void)
 {
 	struct sigaction	sa;
+	struct sigaction	sa_quit;
 
 	sa.sa_handler = signal_handler_hd;
 	sa.sa_flags = 0;
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGINT, &sa, NULL);
+	sa_quit.sa_handler = SIG_IGN;
+	sa_quit.sa_flags = 0;
+	sigemptyset(&sa_quit.sa_mask);
+	sigaction(SIGQUIT, &sa_quit, NULL);
 }
 
 void	setup_signals_hd(void)
--- src/signal/signals_noninteractive.c
+++ src/signal/signals_noninteractive.c
@@ -6,7 +6,7 @@
 /*   By: xiruwang <xiruwang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 19:44:07 by jschroed          #+#    #+#             */
-/*   Updated: 2024/05/26 13:12:49 by jschroed         ###   ########.fr       */
+/*   Updated: 2024/05/26 13:37:21 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,12 @@ static void	sigint_handler_noint(int signum)
 			exit(EXIT_SIGINT);
 		}
 	}
+	if (signum == SIGQUIT)
+	{
+		write(1, "Quit (core dumped)", ft_strlen("Quit (core dumped)"));
+		write(1, "\n", 1);
+		rl_on_new_line();
+	}
 }
 
 void	init_signals_noint(void)
@@ -39,7 +45,7 @@ void	init_signals_noint(void)
 	sa_int.sa_flags = 0;
 	sigemptyset(&sa_int.sa_mask);
 	sigaction(SIGINT, &sa_int, NULL);
-	sa_quit.sa_handler = SIG_IGN;
+	sa_quit.sa_handler = sigint_handler_noint;
 	sa_quit.sa_flags = 0;
 	sigemptyset(&sa_quit.sa_mask);
 	sigaction(SIGQUIT, &sa_quit, NULL);
